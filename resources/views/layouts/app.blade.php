63cb380a2a471060b45370be7d8c2ae960ace258
Maximiliano <maxi.rodriguez94@gmail.com>
2022-09-21 11:55:45 -0300

    Iconos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/539ec73093.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
