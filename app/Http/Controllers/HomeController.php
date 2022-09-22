18973d4188a011f338e21e97e5c631b53d2e9dd3
Maximiliano <maxi.rodriguez94@gmail.com>
2022-09-22 11:54:54 -0300

    Cargar valores de item disenio

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
+use App\Models\Location;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $locations = Location::all();
+        $items  =Item::all();
+
+        return view('home')
+        ->with('locations', $locations)
+        ->with('items', $items);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Cargar Valores') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="location">Ubicacion</label>
+                                <select name="location_id" id="location" class="form-control">
+                                    @foreach ($locations as $location)
+                                        <option value="{{ $location->id }}">{{ $location->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="item">Item</label>
+                                <select name="item_id" id="item" class="form-control">
+                                    @foreach ($items as $item)
+                                        <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="">Valor a cargar</label>
+                                <input type="text" class="form-control" placeholder="00.00">
+                            </div>
+                            <div class="form-group">
+                                <label for="date">Fecha actual</label>
+                                <input type="date" id="date" class="form-control" value="{{ date('Y-m-d') }}"
+                                    disabled>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-2">
+                                Confirmar y cargar valor
+                            </button>
+                    </div>
+                </div>
 
-                    {{ __('You are logged in!') }}
+                <div class="card mt-4">
+                    <div class="card-header">
+                        Ultimos valores cargados
+                    </div>
+                    <div class="car-body">
+                        <table class="table table-hover">
+                            <thead>
+                                <th>Item</th>
+                                <th>Ubicacion</th>
+                                <th>Valor</th>
+                                <th>Fecha carga</th>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
